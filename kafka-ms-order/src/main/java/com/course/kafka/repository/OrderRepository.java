d6d018bd2cfd64cf3397b51d5598d5ecb641ed02
goumze <gm_mishra@hotmail.com>
2026-04-23 11:23:45 +0000

    chore: clear repository contents

--- kafka-ms-order/src/main/java/com/course/kafka/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.course.kafka.repository;
-
-import com.course.kafka.entity.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends CrudRepository<Order,Integer> {
-}
