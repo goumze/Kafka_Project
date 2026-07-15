d362dbc4d1785f5dbee9640cd8771f2f466487ea
goumze <gm_mishra@hotmail.com>
2026-07-15 12:12:32 +0000

    Remove kafka-ms-reward and kafka-ms-storage modules along with their associated files and configurations

--- kafka-ms-order/src/main/java/com/course/kafka/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.course.kafka.repository;
-
-import com.course.kafka.entity.OrderItem;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends CrudRepository<OrderItem,Integer> {
-}
